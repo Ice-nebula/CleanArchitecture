99b1184996d2dedf62412d284767155ab36e50d4
IceBytes <soponhantae@gmail.com>
2022-04-26 11:18:54 +0700

    add new Entities configuration in Infrastructure layer to store all entity configuration
    and add productItemEntityConfiguration class to demonstrate entity config.

---
+++ Src/CleanArchitecture.Infrastructure/Data/EntitiesConfiguration/ProductItemEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.EntitiesConfiguration
+{
+    internal class ProductItemEntityConfiguration : IEntityTypeConfiguration<ProductItem>
+    {
+        public void Configure(EntityTypeBuilder<ProductItem> builder)
+        {
+            builder.Property(x => x.ProductName)
+                .HasMaxLength(250).IsRequired();
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
