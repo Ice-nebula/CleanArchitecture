62222e4c971938f81512110f5e1059008cbbd534
IceBytes <soponhantae@gmail.com>
2022-04-26 11:46:21 +0700

    implement audit entity in SaveChangesAsync method

--- Src/CleanArchitecture.Infrastructure/Data/DataContext.cs
+++ Src/CleanArchitecture.Infrastructure/Data/DataContext.cs
@@ -1,8 +1,10 @@
-﻿using CleanArchitecture.Domain.Entities.Products;
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Entities.Products;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +19,25 @@ namespace CleanArchitecture.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(builder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.DateCreated = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.DateModified = DateTime.Now;
+                        break;
+                } //end switch
+            }//end for.each
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
